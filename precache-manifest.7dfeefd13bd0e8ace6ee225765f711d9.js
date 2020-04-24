a8edecaf4f5f97f138a6b7747f2552e290c3e316
manish <manishshiwlani123@gmail.com>
2020-04-23 21:49:57 -0400

    Updates

---
+++ precache-manifest.7dfeefd13bd0e8ace6ee225765f711d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82879f49cec39137f6f412482ff05efd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "be6dd38c31f23ff55100",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "869fef8b4f75a0abf73b",
+    "url": "/portfolio/static/js/2.8245f6d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8245f6d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be6dd38c31f23ff55100",
+    "url": "/portfolio/static/js/main.01276037.chunk.js"
+  },
+  {
+    "revision": "ec9dbe8eb76dbe993038",
+    "url": "/portfolio/static/js/runtime-main.e1089ece.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
