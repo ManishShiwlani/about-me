a889f6449afce1bbab4d7729452c011f88a79c66
manish <manishshiwlani123@gmail.com>
2020-04-29 20:43:33 -0400

    fixes the position for navigation bar

--- src/components/home/homePage.js
+++ src/components/home/homePage.js
@@ -2,20 +2,20 @@ import React from 'react';
 import './homePage.css';
 
 
-function HomePage() {
+const HomePage = () => {
     return (
         <div>
-            <header>
-                <div className="et-hero-tabs">
-                    <div className="et-hero-tabs-container">
-                        <a className="et-hero-tab" href="#about-me">About Me</a>
-                        <a className="et-hero-tab" href="#technicalprojects">Technical Projects</a>
-                        <a className="et-hero-tab" href="#communityservice">Community Service Projects</a>
-                        <a className="et-hero-tab" href="#contact">Contact</a>
-                        <span className="et-hero-tab-slider"></span>
-                    </div>
+            <div>
+                <div className="et-hero-tabs-container">
+                    <a className="et-hero-tab" href="#about-me">About Me</a>
+                    <a className="et-hero-tab" href="#technicalprojects">Technical Projects</a>
+                    <a className="et-hero-tab" href="#communityservice">Community Service Projects</a>
+                    <a className="et-hero-tab" href="#contact">Contact</a>
                 </div>
-            </header>
+            </div>
+            <div className="name">
+                <h1>Manish Shiwlani</h1>
+            </div>
             <main className="et-main">
                 <div className="et-slide" id="about-me">
                     <h1>About Me</h1>
