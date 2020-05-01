6d10bc857f28b3f878ac3700054fdc71a6635c27
manish <manishshiwlani123@gmail.com>
2020-04-30 22:50:59 -0400

    cleens up styling of the homepage

--- src/components/home/homePage.js
+++ src/components/home/homePage.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import './homePage.css';
-
+import ProfilePic from './profilepic.png';
 
 const HomePage = () => {
     return (
         <div>
-            <div>
-                <div className="et-hero-tabs-container">
-                    <a className="et-hero-tab" href="#about-me">About Me</a>
-                    <a className="et-hero-tab" href="#technicalprojects">Technical Projects</a>
-                    <a className="et-hero-tab" href="#communityservice">Community Service Projects</a>
-                    <a className="et-hero-tab" href="#contact">Contact</a>
-                </div>
-            </div>
-            <div className="name">
-                <h1>Manish Shiwlani</h1>
+            <div className="et-hero-tabs-container">
+                <a className="et-hero-tab" href="#about-me">About Me</a>
+                <a className="et-hero-tab" href="#technicalprojects">Technical Projects</a>
+                <a className="et-hero-tab" href="#communityservice">Community Service Projects</a>
+                <a className="et-hero-tab" href="#contact">Contact</a>
             </div>
             <main className="et-main">
                 <div className="et-slide" id="about-me">
-                    <h1>About Me</h1>
+                    <h1>Manish Shiwlani</h1>
                     <div className="flex">
                         <div>
                             <img
-                                src={process.env.PUBLIC_URL + '/profile.png'} alt="Manish" />
+                                src={ProfilePic} alt="Manish" />
                             <h2>Manish Shiwlani</h2>
                         </div>
                     </div>
